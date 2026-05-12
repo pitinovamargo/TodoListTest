6ccb1736c5d1cc1aff278a0b191bd06c947e89e6
Margarita Pitinova <123267715+pitinovamargo@users.noreply.github.com>
2026-05-12 22:11:29 +0200

    create edit button functionality

--- TodoListTest/TodoListViewModel.swift
+++ TodoListTest/TodoListViewModel.swift
@@ -10,6 +10,7 @@ import Combine
 final class TodoListViewModel: ObservableObject {
     @Published var todos: [Todo] = Todo.sampleData
     @Published var searchQuery: String = ""
+    @Published var path: [Todo] = []
 
     func toggle(_ todo: Todo) {
         guard let index = todos.firstIndex(where: { $0.id == todo.id }) else { return }
@@ -18,5 +19,10 @@ final class TodoListViewModel: ObservableObject {
 
     func delete(_ todo: Todo) {
         todos.removeAll { $0.id == todo.id }
+    }
+
+    func update(_ todo: Todo) {
+        guard let index = todos.firstIndex(where: { $0.id == todo.id }) else { return }
+        todos[index] = todo
     }
 }
